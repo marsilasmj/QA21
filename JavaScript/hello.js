00337ac024335804eaa3b2b5184c03bec1d4d0b4
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2023-12-14 06:41:28 +0300

    javaScript

--- JavaScript/hello.js
+++ JavaScript/hello.js
@@ -1,8 +1,86 @@
-console.log("hello world")
-x=10;
+//  comment
+// single line comment
+
+/* muliti line commnet */
+
+// JAVASCRIPT VARIABLES
+x = 10
+
+// x is variable
+// 10 is value (data)
+
+
+// class work 
+
+name = "craft"   // variable = name, value = craft 
+age = 25         // variable = age, value = 25
+sub = "javaScript" // variable = sub, value = javaScript
+batch = 21        // variable = batch, value = 21
+
+//  Declare a variable
+// 4 Ways to Declare a JavaScript Variable:
+// Using nothing 
+// Using var
+// Using let
+// Using const
+
+// keyword.
+
+var name1 = "efrem";
+let name2 = "new name"
+const name3 = "craftclass"
+
+// we can - nothing,var,let,const
+// rule for naming varabile
+// 1. letters [a-z, A-Z], digits [0,9], underscores[_], and dollar signs [$]. 
+       // not use this => @ # % ^ & * () - = +
+// 2. we can't start with digits [0,9]
+      // not use this 1name
+// 3. case sencetive X and x diff variable
+// 4. Reserved words keyword.
+    //not use class if else while for let var const
+
+// class = 10; Reserved word not correct.
+// 1student = "efrem"; not correct
+// @craft = student; not correct
+// $name = "Efrem";  correct answ
+// ^12 = "num"  not correct.
+
+
+// Assignment Operator (=) we use for assing value for variable
+ name =  10
+// var
+var myName;   // declartion
+console.log(myName)
+var myName;  // redecalation
 {
-    x=20
-    y=10
-    z=x+y;
-    console.log(y)
+   var myname1 = "efrem"
 }
+// access out of scope
+console.log(myname1)
+var age = 10
+age = 20  // reassignement 
+
+var address;  // defin with out value
+// let 
+let Z
+  // redecalation
+{
+    let data = "efrem"
+}
+//we can't accress out of scope
+
+
+let reassignement
+
+let y;
+// let we can define val with out value
+
+const t = 10
+// const t  not correct redeclartion
+{
+    const w = 10
+    console.log(w)  
+}
+const q =10
+
